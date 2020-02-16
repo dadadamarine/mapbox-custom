5ea7c5bdeceeb2da825a68ac6bcd64e3222685bd
dadadamarine <dadadamarine@naver.com>
2020-02-16 10:36:48 +0900

    add : 외부 GsoJson데이터 표현하기
     외부에서 가져온 GeoJson데이터로 소스를 추가해서, 그 소스 레이어로 등록해서 표현하였다.
    
     see also : #1
    
     check contents : done
     check format, imports : done

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div id = "map"></div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -22,5 +25,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import mapboxgl from 'mapbox-gl'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGFkYWRhbWFyaW5lIiwiYSI6ImNqeWw0cXNleTA0bHUzaHM4eXZ4eTB2YTMifQ.GhEeiW7qRb71WkyW3P_ZGg';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
+
+map.on('load', function () {
+    map.addSource('sickPerson', {
+        type: 'geojson',
+        data: 'http://localhost:8080/virus/locationInfo/directZone?date=2020-01-24',
+    });
+    map.addLayer(
+        {
+            'id': 'sickPerson',
+            'type': 'fill',
+            'source': 'sickPerson',
+            'layout': {
+                'visibility': 'visible'
+            },
+            'paint': {
+                'fill-color': 'red'
+            }
+        }
+    )
+});
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
